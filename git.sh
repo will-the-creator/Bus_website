d6d8e52ab9b923e78fcb376d685d75b66447536b
will-the-creator <hoopa05.cascade@icloud.com>
2025-07-28 10:52:09 -0600

    rm git.sh

--- git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git add . || { echo "git add failed"; exit 1; }
-read -p "commit message : " msg
-git commit -m "$msg" || { echo "git commit failed"; exit 1; }
-echo "message committed..."
-git push origin main || { echo "git push failed"; exit 1; }
-echo "updated"
